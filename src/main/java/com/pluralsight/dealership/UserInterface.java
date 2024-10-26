558af29229e2b4d6518a60dd2b87f2aa80d20af8
Osmig_Torres <torresosmig@gmail.com>
2024-10-25 20:50:24 -0700

    delete car method finished

--- src/main/java/com/pluralsight/dealership/UserInterface.java
+++ src/main/java/com/pluralsight/dealership/UserInterface.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
     private static int counter = 1;
     private static final Scanner scanner = new Scanner(System.in);
+    private static boolean isInFile = true;
 
     public static void displayVehicle(int counter, Vehicle vehicle){
         System.out.print("\n\n----- Vehicle " + counter + " -----" +
@@ -23,8 +25,59 @@ public class UserInterface {
                 "\n price: " + vehicle.getPrice() );
     }
 
-    public static void removeVehicle() {
-       //List<Vehicle> vehicles = Dealership.getVehicleByPrice();
+    public static void removeVehicle() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("inventory.txt"));
+        List<Vehicle> vehiclesInventory = Dealership.getAllVehicles();
+        List<Vehicle> newCarInventory = new ArrayList<>();
+        System.out.print("Please enter vin # of vehicle to delete: ");
+        String vin = scanner.nextLine().trim();
+//        if(vehiclesInventory.get(0).getVin().equalsIgnoreCase(vin)){
+//            System.out.println("Vin #" + vehiclesInventory.getFirst().getVin() + " has been deleted");
+//        }else {
+//            System.out.println("Vin #" + vin + " is not in the data base");
+//        }
+
+        for (Vehicle vehicle : vehiclesInventory){
+            if (!vehicle.getVin().equals(vin)) {
+//                System.out.println("Vehicle with vin #" + vin + " does not exist in database");
+                isInFile = false;
+
+            }else {
+                isInFile = true;
+            }
+        }
+
+        if (!isInFile){
+            System.out.println("Vehicle with vin #" + vin + " not found in database");
+        }
+
+        if(isInFile){ // check vin # match
+            for (Vehicle vehicle : vehiclesInventory){
+                if(!vehicle.getVin().equalsIgnoreCase(vin)){
+                    newCarInventory.add(vehicle);
+                }
+            }
+
+            bufferedWriter.write("D & B Used Cars|111 Old Benbrook Rd|817-555-5555");
+            for(Vehicle vehicle : newCarInventory){
+                Vehicle car = new Vehicle(vehicle.getVin(),vehicle.getYear(), vehicle.getMake(), vehicle.getModel(),
+                        vehicle.getVehicleType() ,vehicle.getColor() , vehicle.getOdometer(), vehicle.getPrice() );
+
+                bufferedWriter.write("\n" + car.getVin() + "|" + car.getYear() + "|" + car.getMake() + "|" +
+                        car.getModel() + "|" + car.getVehicleType() + "|" + car.getColor() + "|" + car.getOdometer() + "|" +
+                        car.getPrice());
+            }
+            System.out.println("Vehicle with vin #" + vin + " has been deleted from database");
+            bufferedWriter.close();
+
+        }
+        /*
+            // Testing if it is not adding the unwanted vehicle to our new array
+            for (int i = 0; i < newCarInventory.size(); i++) {
+            System.out.println(newCarInventory.get(i).toString());
+            }
+        */
+
     }
 
     public static void addVehicle() throws IOException {
